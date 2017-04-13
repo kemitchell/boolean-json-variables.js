ba1c7b55c012707a5c1b48ae50f293290f85a170
Robert Herhold <reherhold@gmail.com>
2017-04-13 08:40:22 -0400

    Check if expression.(not|and|or) are truthy before continuing

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ function booleanJSONVariables(expression, results) {
     if (results.indexOf(expression) === -1) {
       results.push(expression) }
     return results }
-  else if ('not' in expression) {
+  else if (expression.not) {
     return booleanJSONVariables(expression.not, results) }
-  else if ('and' in expression) {
+  else if (expression.and) {
     return expression.and
       .reduce(
         function(results, expression) {
           return booleanJSONVariables(expression, results) },
         results) }
-  else if ('or' in expression) {
+  else if (expression.or) {
     return expression.or
       .reduce(
         function(results, expression) {
